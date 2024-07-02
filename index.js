7114165ed18ab94d8ccdcf5514bfcbaff8fbfb46
mbsj145 <you@example.com>
2024-07-02 10:25:14 +0500

    updated

--- index.js
+++ index.js
@@ -17,13 +17,10 @@ database.getConnection();
 app.use(express.static(__dirname + 'public'));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(bodyParser.json({ limit: '50mb' }));
-app.use('/uploads', express.static('public/uploads'));
-app.use('/profiles', express.static('public/profiles'));
-app.use('/bg', express.static('public/bg'));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(bodyParser.json({ limit: '50mb' }));
 require('./routes')(app);
 app.get('*', (req, res) => res.sendFile(path.normalize(__dirname + '/public/index.html')));
 
 
-app.listen(port_no, () => console.log(`NFT is serving on ${port_no}`));
+app.listen(port_no, () => console.log(`0x Burn is serving on ${port_no}`));
